b85786438ee060eb9549de5b96ad81fdd67cbba3
sujinfeng2 <461068980@qq.com>
2019-10-10 08:25:21 +0800

    添加部分数据接口

--- index.js
+++ index.js
@@ -90,6 +90,96 @@ app.post('/citys', urlencodedParser, function(request, response){
     });
 });
 
+app.post('/test1', urlencodedParser, function(request, response){
+  // 输出 JSON 格式
+    
+    data =[ 
+       {"yy": "摄像头故1障", "dd": "公sadfsdfsdfsd5园", "sj": "2019.10.10"}, 
+       {"yy": "摄像4头故障", "dd": "公2园", "sj": "2019.10.10"}, 
+       {"yy": "摄像头故1障", "dd": "公UUu园", "sj": "2019.10.10"}, 
+       {"yy": "摄像头3故障", "dd": "公546456园", "sj": "2019.10.10"}, 
+       {"yy": "摄像3头故2障", "dd": "公2园", "sj": "2019.10.10"}, 
+       {"yy": "摄像头故3障", "dd": "公7园", "sj": "2019.10.10"}, 
+       {"yy": "摄像头5故障", "dd": "公8园", "sj": "2019.10.10"}, 
+       {"yy": "摄像头故障", "dd": "公8园2", "sj": "2019.10.11"}
+    ];
+   console.log(data);
+  //  response.end(JSON.stringify(data));
+   response.json(data);
+});
+
+ app.post('/test2', urlencodedParser, function(request, response){
+  // 输出 JSON 格式
+    
+    data = {"a": "5", "b": "6","c":"优"}, 
+   console.log(data);
+  //  response.end(JSON.stringify(data));
+   response.json(data);
+});
+
+app.post('/test3', urlencodedParser, function(request, response){
+  // 输出 JSON 格式
+    
+    data =[
+    {
+        "value": "三全鲜食（北新泾店）",
+        "address": "长宁区新渔路144号"
+    },
+    {
+        "value": "Hot honey 首尔炸鸡（仙霞路）",
+        "address": "上海市长宁区淞虹路661号"
+    },
+    {
+        "value": "新旺角茶餐厅",
+        "address": "上海市普陀区真北路988号创邑金沙谷6号楼113"
+    },
+    {
+        "value": "泷千家(天山西路店)",
+        "address": "天山西路438号"
+    },
+    {
+        "value": "胖仙女纸杯蛋糕（上海凌空店）",
+        "address": "上海市长宁区金钟路968号1幢18号楼一层商铺18-101"
+    },
+    {
+        "value": "贡茶",
+        "address": "上海市长宁区金钟路633号"
+    },
+    {
+        "value": "豪大大香鸡排超级奶爸",
+        "address": "上海市嘉定区曹安公路曹安路1685号"
+    },
+    {
+        "value": "茶芝兰（奶茶，手抓饼）",
+        "address": "上海市普陀区同普路1435号"
+    },
+    {
+        "value": "十二泷町",
+        "address": "上海市北翟路1444弄81号B幢-107"
+    },
+    {
+        "value": "星移浓缩咖啡",
+        "address": "上海市嘉定区新郁路817号"
+    },
+    {
+        "value": "阿姨奶茶/豪大大",
+        "address": "嘉定区曹安路1611号"
+    },
+    {
+        "value": "新麦甜四季甜品炸鸡",
+        "address": "嘉定区曹安公路2383弄55号"
+    },
+    {
+        "value": "Monica摩托主题咖啡店",
+        "address": "嘉定区江桥镇曹安公路2409号1F，2383弄62号1F"
+    }
+];
+   console.log(data);
+  //  response.end(JSON.stringify(data));
+   response.json(data);
+});
+
+
 app.listen(8888);
 
 
